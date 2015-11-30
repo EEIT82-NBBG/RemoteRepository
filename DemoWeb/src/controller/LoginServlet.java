d7b04b77b23ae1b7b0322a65858d34f770224ba9
陳毅鴻 <A9006066@kimo.com>
2015-11-30 16:20:24 +0800

    Cahjfhdshdsfjds

--- DemoWeb/src/controller/LoginServlet.java
+++ DemoWeb/src/controller/LoginServlet.java
@@ -68,7 +68,7 @@ public class LoginServlet extends HttpServlet {
 			String path = request.getContextPath();
 			response.sendRedirect(path+"/index.jsp");
 		}
-		System.out.print("xxxx");
+		System.out.print("3665444");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
