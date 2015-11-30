5d1886ed20dc0e90cdbffa52fe3f64654c7309b2
鄭雅竹 <chu@yahoo.com.tw>
2015-11-30 16:47:28 +0800

    546

--- DemoWeb/src/controller/LoginServlet.java
+++ DemoWeb/src/controller/LoginServlet.java
@@ -62,7 +62,7 @@ public class LoginServlet extends HttpServlet {
 			response.sendRedirect(path+"/index.jsp");
 		}
 
-		System.out.print("aaaa");
+		System.out.print("fffff");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
